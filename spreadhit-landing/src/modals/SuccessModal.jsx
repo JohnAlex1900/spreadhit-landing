d8e31ac6ba6bc58e22e2ff94ca8650e1810f0aa7
John Irungu <johnirungu977@gmail.com>
2024-09-27 15:59:37 +0300

    Final Changes

--- spreadhit-landing/src/modals/SuccessModal.jsx
+++ spreadhit-landing/src/modals/SuccessModal.jsx
@@ -1,30 +1,27 @@
 import "react";
 import { Modal, Button, Image } from "react-bootstrap";
 import PropTypes from "prop-types";
-import listImage from "../assets/added-list.png";
+import excitedImage from "../assets/excited.png";
 
 const SuccessModal = ({ show, handleClose }) => {
   return (
     <Modal show={show} onHide={handleClose} centered>
       <Modal.Header closeButton>
-        <Modal.Title>You&apos;re now on our waitlist!</Modal.Title>
+        <Modal.Title>Spreadhit is Coming Soon</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>
-          Thank you for joining the SpreadHit waitlist. You&apos;re one step
-          closer to creating amazing AI-generated videos!
-        </p>
         <div className="text-center">
-          <Image src={listImage} fluid roundedCircle />
+          <p>Are You Excited?!</p>
+          <Image src={excitedImage} fluid />
         </div>
-        <p className="mt-4">
-          Stay tuned! We&apos;ll notify you as soon as SpreadHit is ready to
-          launch.
-        </p>
       </Modal.Body>
       <Modal.Footer className="justify-content-center">
-        <Button variant="success" onClick={handleClose}>
-          Okay
+        <Button
+          variant="info"
+          className="w-50 text-white"
+          onClick={handleClose}
+        >
+          Yes
         </Button>
       </Modal.Footer>
     </Modal>
